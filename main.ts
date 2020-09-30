ed1f763f42b9361db55b5f3ab4e00ff45f7dcfaa
Ahmed-Ali-0 <71400128+Ahmed-Ali-0@users.noreply.github.com>
2020-09-30 14:28:24 -0400

    add block

--- main.ts
+++ main.ts
@@ -2,13 +2,13 @@
 // Created Date: Sep30, Tuesday
 // 
 // This program use  Neopines traffic light.
-let strip = neopixel.create(DigitalPin.P16, 1, NeoPixelMode.RGB)
+let strip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
 strip.clear()
 strip.show()
 basic.showIcon(IconNames.Yes)
-strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-strip.show()
+strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
 basic.pause(4000)
+strip.show()
 basic.showIcon(IconNames.Happy)
 strip.clear()
 strip.show()
